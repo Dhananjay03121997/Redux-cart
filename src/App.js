6420fc7ef69e5b432b4193ba823520f79f8183a1
unknown <dhananjay.ivision@gmail.com>
2021-04-11 13:06:27 +0530

    Added products

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import Product from './component/product';
+import data from './data.json'
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      products : data.product
+    }
+  }
   render(){
     return (
       <div className="App">
         <header className="App-header">
-
+          
         </header>
+        <div className="contain">
+          <div className="products"> 
+           <Product products={this.state.products}></Product>
+          </div>
+          <div className="Cart"> CART</div>
+        </div>
       </div>
     );
   }
